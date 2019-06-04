246b47b0608fcb281be10940993c429e2f06f121
Ivan Liang <ivanwenliang@gmail.com>
2019-06-03 23:29:54 -0700

    Add clipboard functionality and copy overlay

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "../styles/ColorBox.css";
 
 class ColorBox extends Component {
+  state = {
+    isCopied: false
+  };
+  // Show overlay for 1.5s after color is copied to clipboard
+  changeCopyState = () => {
+    this.setState({ isCopied: true }, () => {
+      setTimeout(() => this.setState({ isCopied: false }), 1500);
+    });
+  };
   render() {
     const { name, background } = this.props;
+    const { isCopied } = this.state;
     return (
-      <div style={{ background }} className="ColorBox">
-        <div className="copy-container">
-          <div className="box-content">
-            <span>{name}</span>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div style={{ background }} className="ColorBox">
+          <div
+            style={{ background }}
+            // Only show overlay when copied is true
+            className={`copy-overlay ${isCopied && "show"}`}
+          />
+          <div className={`copy-msg ${isCopied && "show"}`}>
+            <h1>Copied!</h1>
+            <p>{this.props.background}</p>
           </div>
-          <button className="copy-button">Copy</button>
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
+          </div>
+          <span className="see-more">More</span>
         </div>
-        <span className="see-more">More</span>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
