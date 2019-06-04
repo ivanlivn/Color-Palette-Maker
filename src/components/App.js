71682bfb66b03abfaf55df0a31fef49a10948652
Ivan Liang <ivanwenliang@gmail.com>
2019-06-04 14:38:46 -0700

    Add routing to different palettes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import starterPalettes from "../starterPalettes";
 import { generatePalette } from "../colorHelpers";
 
-function App() {
-  return (
-    <div>
-      <Palette palette={generatePalette(starterPalettes[4])} />
-    </div>
-  );
+class App extends Component {
+  findPalette = id => {
+    return starterPalettes.find(function(palette) {
+      return palette.id === id;
+    });
+  };
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
 }
 
 export default App;
