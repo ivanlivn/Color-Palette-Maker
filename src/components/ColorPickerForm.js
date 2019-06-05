b9e811b43bf2847613891e6e3302286d51f2f135
Ivan Liang <ivanwenliang@gmail.com>
2019-06-05 10:52:51 -0700

    Fix form styles

--- src/components/ColorPickerForm.js
+++ src/components/ColorPickerForm.js
@@ -3,23 +3,7 @@ import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { ChromePicker } from "react-color";
 import { withStyles } from "@material-ui/core/styles";
-
-const styles = {
-  picker: {
-    width: "100% !important",
-    marginTop: "2rem"
-  },
-  addColor: {
-    width: "100%",
-    padding: "1rem",
-    marginTop: "1rem",
-    fontSize: "2rem"
-  },
-  colorNameInput: {
-    width: "100%",
-    height: "70px"
-  }
-};
+import styles from "../styles/ColorPickerFormStyles";
 
 class ColorPickerForm extends Component {
   state = {
@@ -89,7 +73,7 @@ class ColorPickerForm extends Component {
             variant="contained"
             color="primary"
             disabled={paletteIsFull}
-            className={classes.adColor}
+            className={classes.addColor}
             style={{
               backgroundColor: paletteIsFull ? "grey" : currentColor
             }}
---
+++ src/styles/NewPaletteFormStyles.js
@@ -0,0 +1,62 @@
+import { DRAWER_WIDTH } from "../constants";
+const drawerWidth = DRAWER_WIDTH;
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    height: "100vh"
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    display: "flex",
+    alignItems: "center"
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  content: {
+    flexGrow: 1,
+    height: "calc(100vh - 64px)",
+    padding: 0,
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  },
+  container: {
+    fontFamily: "Roboto",
+    width: "90%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttons: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-evenly"
+  },
+  button: {
+    width: "40%"
+  }
+});
+
+export default styles;
